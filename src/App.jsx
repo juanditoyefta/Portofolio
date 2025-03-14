9f33c049e1695037fa2028bc835d298b26b18608
juandito yefta <yeftapriatama@gmail.com>
2025-03-14 09:59:28 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { LoadingScreen } from './components/Loadingscreen'
+
+function App() {
+  return (
+    <>
+      <LoadingScreen/>
+    </>
+  )
+}
+
+export default App
