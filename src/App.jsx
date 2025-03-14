b0cfcec57b15c76fcffd1d64b016bce238b90476
juandito yefta <yeftapriatama@gmail.com>
2025-03-14 16:10:23 +0700

    Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
-import { LoadingScreen } from './components/Loadingscreen'
+import { useState } from 'react';
+import './App.css';
+import { LoadingScreen } from './components/Loadingscreen';
+import "./index.css";
 
 function App() {
+  const[isLoaded, setIsLoaded] = useState(false)
+
   return (
-    <>
-      <LoadingScreen/>
-    </>
-  )
+    <>{!isLoaded &&<LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+    <div className= {`min-h-screen transition-opacity duration-700 ${
+      isLoaded ? "opacity-100" : "opacity-0"
+    } bg-black text-gray-100`}>
+    
+    </div></>
+  );
 }
 
-export default App
+export default App;
