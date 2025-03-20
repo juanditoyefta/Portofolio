002bf1f42ac0f96378b18b90c11c37c2a98baf9d
juandito yefta <yeftapriatama@gmail.com>
2025-03-20 15:52:02 +0700

    Contact

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,7 +1,10 @@
+import { RevealOnScroll } from "./RevealOnScroll"
+
 export const Home =() => {
     return <section 
     id="home" 
     className="min-h-screen flex items-center justify-center relative">
+        <RevealOnScroll>
         <div className="text-center z-10 px-4">
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">
                 Juandito Yefta Priatama
@@ -24,5 +27,6 @@ export const Home =() => {
                 </a>
             </div>
         </div>
+        </RevealOnScroll>
     </section>
 }
